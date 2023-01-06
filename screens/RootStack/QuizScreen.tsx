2cc49851a1be8193c30768c605155cc97c1c5b06
ibrahimmesan <ibrahinmesan@gmail.com>
2023-01-06 17:28:24 +0100

    progress

--- screens/RootStack/QuizScreen.tsx
+++ screens/RootStack/QuizScreen.tsx
@@ -1,52 +1,64 @@
 import { Text, View } from '../../components/Themed';
 import {styles} from "../../constants/Styles";
-import React, {useState} from "react";
-import YoutubePlayer from "react-native-youtube-iframe";
+import React, {useEffect, useRef, useState} from "react";
+// import YoutubePlayer from "react-native-youtube-iframe";
 import Layout from "../../constants/Layout";
 import {Modal, StyleSheet} from "react-native";
-// import WebView from "react-native-webview";
+import WebView from "react-native-webview";
 
 
 export default function QuizScreen() {
     const [ready, setReady] = useState<boolean>(false)
-    const [visible, setVisible] = useState<boolean>(true)
+    // const [visible, setVisible] = useState<boolean>(true)
 
     return (
-        //     <View style={{display: ready? 'flex':'none'}}>
-        //         <WebView
-        //             onLoadEnd={() => {
-        //                 console.log('finish')
-        //                 setReady(true)
-        //             }}
-        //             source={{ uri: 'https://www.youtube.com/embed/UDIQwGb-4YQ' }}
-        //             style={styles.video}
-        //             allowsFullscreenVideo
-        //             mediaPlaybackRequiresUserAction={false}
-        //         />
-        //     </View>
         <View style={styles.container}>
-                    <YoutubePlayer
-                        webViewStyle={styles.video}
-                        play={true}
-                        onReady={() => setReady(true)}
-                        mute={false}
-                        videoId={"UDIQwGb-4YQ"}
-                        onChangeState={() => setReady(true)}
-                        height={Layout.window.height}
-                        width={Layout.window.height}
-                        webViewProps={{
-                            allowsFullscreenVideo: true,
-                            mediaPlaybackRequiresUserAction: false,
-                        }}
-                        // initialPlayerParams={{controls: false}}
-                    />
-                    <View style={{
-                        position: 'absolute',
-                        backgroundColor: 'red',
-                        width: '100%', bottom: 0, height: 100}}
-                    />
-
+            {/*<View style={{display: ready? 'flex':'none'}}>*/}
+                <WebView
+                    onLoadEnd={() => setReady(true)}
+                    source={{ uri: 'https://www.youtube.com/embed/UDIQwGb-4YQ?autoplay=1' }}
+                    style={styles.video}
+                    allowsFullscreenVideo={false}
+                    allowsInlineMediaPlayback={true}
+                    mediaPlaybackRequiresUserAction={false}
+                    javaScriptEnabled={true}
+                    onMessage={event => console.log(event)}
+                    injectedJavaScriptBeforeContentLoaded={"document.getElementsByClassName('ytp-chrome-bottom')[0].style.display='none'"}
+                    injectedJavaScript={"document.getElementsByClassName('ytp-chrome-bottom')[0].style.display='none'"}
+                    userAgent={"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"}
+                />
+                <View style={{
+                    position: 'absolute',
+                    backgroundColor: 'transparent',
+                    width: '100%', bottom: 0, height: '100%'}}
+                >
+                    <View style={{width: '100%', height: 40, backgroundColor: 'red'}}/>
+                    <View style={{width: '89%', height: 40, backgroundColor: 'blue', marginTop: 200}}/>
+                </View>
+            {/*</View>*/}
         </View>
+        // <View style={styles.container}>
+        //             <YoutubePlayer
+        //                 webViewStyle={styles.video}
+        //                 play={true}
+        //                 onReady={() => setReady(true)}
+        //                 mute={false}
+        //                 videoId={"UDIQwGb-4YQ"}
+        //                 onChangeState={() => setReady(true)}
+        //                 height={Layout.window.height}
+        //                 width={Layout.window.height}
+        //                 webViewProps={{
+        //                     allowsFullscreenVideo: true,
+        //                     mediaPlaybackRequiresUserAction: false,
+        //                 }}
+        //                 // initialPlayerParams={{controls: false}}
+        //             />
+        //             <View style={{
+        //                 position: 'absolute',
+        //                 backgroundColor: 'red',
+        //                 width: '100%', bottom: 0, height: 100}}
+        //             />
+        // </View>
     );
 }
 
